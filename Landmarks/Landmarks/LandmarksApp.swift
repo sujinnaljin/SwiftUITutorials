6a66ebebf44be75a04f1ef12ba255a0a1d048e74
sujin <rkdthd1234@naver.com>
2021-07-22 00:07:44 +0900

    Adopt the Model Object in Your Views

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    //@StateObject 를 사용하여 지정된 속성에 대한 모델 object를 앱 life cycle 동안 한 번만 초기화. 이는 앱 인스턴스에서 속성을 사용하는 경우뿐만 아니라 뷰에서도 사용할 경우 해당
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
