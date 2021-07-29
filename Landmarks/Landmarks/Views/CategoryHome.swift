8d485897eb5120c8e85546e2ebe5d46e288d973b
sujin <rkdthd1234@naver.com>
2021-07-29 21:13:59 +0900

    Add a Category View

---
+++ Landmarks/Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 강수진 on 2021/07/29.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello")
+                .navigationTitle("Featured")
+        }
+        
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
