cc6af8714125d07b170f54a290ae190ead07b51e
sujin <rkdthd1234@naver.com>
2021-08-03 23:24:02 +0900

    Create the Detail View

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by 강수진 on 2021/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
