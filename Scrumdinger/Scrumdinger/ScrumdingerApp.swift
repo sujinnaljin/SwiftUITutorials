356e7f5a9b365a7e1ed36356eabe5f1cfaae9a66
sujin <rkdthd1234@naver.com>
2021-08-05 20:38:42 +0900

    Create a Project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 강수진 on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
